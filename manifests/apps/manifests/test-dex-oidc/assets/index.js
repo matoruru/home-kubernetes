e8acda6677c82317c9c9c729edf0b0f2ae33eff8
matoruru <matoruru@shgt.jp>
2024-08-16 04:15:14 +0900

    Revert "modify: Sample application"
    This reverts commit 09a9471262861786047e42eb439eee2e3169925a.

--- manifests/apps/manifests/test-dex-oidc/assets/index.js
+++
@@ -1,7 +0,0 @@
-import { SecretClient } from "@azure/keyvault-secrets";
-import { DefaultAzureCredential } from "@azure/identity";
-const credential = new DefaultAzureCredential();
-const vaultUrl = 'https://home-k8s-keyvault.vault.azure.net/'
-const client = new SecretClient(vaultUrl, credential);
-const secret = client.getSecret('mytestsecret')
-console.log({ secret })
